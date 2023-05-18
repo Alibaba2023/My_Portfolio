0ccf49e1975528790851a67f1b8225e86263db8d
AliBaba <ali7090370@gmail.com>
2023-05-19 04:18:30 +0430

    Popup complete

--- index.js
+++ index.js
@@ -138,7 +138,8 @@ const featureCardMultiPost = (data, i) => `
     ${data.tech
     .map(
       (detail) => `<li><a class="language" href="#"><p class="language-feature drak-color">${detail}</p></a></li>`)
-    .join("")}
+    .join("")
+    }
     </ul>
     <a href="${data.livelink}"
     content-id="${i}" class="btn-popup btn btn-inline-block recent-btn">
@@ -163,8 +164,9 @@ const featureCardFirst = (data, i) => `
     <ul class="languages-list flex">
     ${data.tech
     .map(
-      (detail) =>`<li><a class="language" href="#"><p class="language-feature">${detail}</p></a></li>`)
-    .join("")}
+      (detail) => `<li><a class="language" href="#"><p class="language-feature">${detail}</p></a></li>`)
+    .join("")
+    }
     </ul>
     </div>
     <a href="${data.livelink}"
@@ -183,8 +185,9 @@ const popupWindow = (data) => `
     <ul class="languages-list flex padding-bottom">
     ${data.tech
     .map(
-      (detail) =>`<li><a class="language" href="#"><p class="drak-color">${detail}</p></a></li>`)
-    .join("")}
+      (detail) => `<li><a class="language" href="#"><p class="drak-color">${detail}</p></a></li>`)
+    .join("")
+    }
     </ul>
     <div class="multi-post">
     <div class="recent-work-img-box padding-bottom">
